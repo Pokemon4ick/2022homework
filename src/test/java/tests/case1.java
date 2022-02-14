45de0081b0a48792bd5a316859b866b5ea4a1e83
EGOR_DUBOVIK <dubovikegor228@gmail.com>
2022-02-15 00:39:44 +0300

    Предусловие

--- src/test/java/tests/case1.java
+++ src/test/java/tests/case1.java
@@ -1,9 +1,16 @@
 package tests;
 
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class case1 {
 private static WebDriver driver;
@@ -12,10 +19,21 @@ private static WebDriver driver;
     public static void init() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
+
     }
+
+    @AfterAll
+    public static void teardown() {
+        driver.quit();
+    }
+
     @Test
     public void test1() {
         driver.get("http://google.com");
-        driver.quit();
+        driver.findElement(By.cssSelector("input")).sendKeys("Калькулятор", Keys.ENTER);
+        driver.findElement(By.cssSelector("[jsname=lVjWed]")).click();
+        assertEquals(4, 3, driver.findElements(By.cssSelector("[jsname=ubtiRe]")).size());
+        assertEquals(1,driver.findElements(By.cssSelector("[jsname=a1lrmb]")).size());
+        assertEquals(2,details);
     }
-}
+    }
