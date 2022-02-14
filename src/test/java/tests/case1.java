13442f3d4bf93579c1475e521b0d0597850178cb
EGOR_DUBOVIK <dubovikegor228@gmail.com>
2022-02-14 22:43:15 +0300

    Предусловие

---
+++ src/test/java/tests/case1.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class case1 {
+private static WebDriver driver;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void test1() {
+        driver.get("http://google.com");
+        driver.quit();
+    }
+}
